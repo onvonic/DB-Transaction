d8749315671eb3cd8d078313493f885cbaa6f661
onvonic <123437632+onvonic@users.noreply.github.com>
2023-12-01 11:02:38 +0700

    Create dbtransaction.php

---
+++ dbtransaction.php
@@ -0,0 +1,26 @@
+DB::beginTransaction();
+
+try {
+
+    #1 DB::table() ... ... 
+    #2 DB::table() ... ... 
+
+    DB::commit();
+
+    $output = [
+        'status' => true,
+        'message' => "Success"
+    ];
+
+    return response()->json($output, 200);
+    
+} catch (\Exception $e) {
+
+    DB::rollback();
+    $output = [
+        'status' => false,
+        'message' => 'System error: ',
+        'error' => $e->getMessage()
+    ];
+    return response()->json($output, 200);
+}
